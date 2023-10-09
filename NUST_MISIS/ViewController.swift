bd7436b1804f6c90242de9619141841c85f6bc33
G7Khrmvvv <khramovgleb40@gmail.com>
2023-10-09 08:25:36 +0300

    lesson2

--- NUST_MISIS/ViewController.swift
+++ NUST_MISIS/ViewController.swift
@@ -5,15 +5,20 @@
 //  Created by imac on 29.09.2023.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func askButtonPrssed(_ sender: UIButton) {
+        imageView.image = ballArray.randomElement()!
+    }
+    
+    var ballArray = [UIImage(named: "ball1"), UIImage(named: "ball2"), UIImage(named: "ball3"), UIImage(named: "ball4"), UIImage(named: "ball5")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.image = UIImage.init(named: "ball1")
     }
-
-
 }
-
